fe81acf0d8c956144568be0bbe8d126eb0d03a7e
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2019-05-03 00:40:48 -0500

    Hibernate config

--- etc/HibernateUtil.java
+++ etc/HibernateUtil.java
@@ -1,5 +1,7 @@
 package ec.cjpq.fe.util;
 
+import java.util.Properties;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -10,8 +12,16 @@ public class HibernateUtil {
 	private static SessionFactory buildSessionFactory() {
 		try {
             
+            Properties properties = new Properties();
+            try {
+                properties.load(ClassLoader.getSystemClassLoader().getResourceAsStream("dbConnection.properties"));
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+
 			// Create the SessionFactory from hibernate.cfg.xml
-			return new Configuration().configure().buildSessionFactory();
+			//return new Configuration().configure().buildSessionFactory();
+			return new Configuration().configure("hibernate.cfg.xml").addProperties(properties).buildSessionFactory();
 		} catch (Throwable ex) {
 			// Make sure you log the exception, as it might be swallowed
 			System.err.println("Initial SessionFactory creation failed." + ex);
